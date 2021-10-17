a3b281380c8fc8a4fb54463149e5527567b91971
navi-singh <33882768+navi-singh@users.noreply.github.com>
2021-10-16 20:46:02 -0500

    Update 0001_two_sum.java

--- 0001_two_sum.java
+++ 0001_two_sum.java
@@ -9,6 +9,9 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
+Time: O(n)
+Space: O(n)
 */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
