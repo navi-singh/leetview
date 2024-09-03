293ec07456f22aa21b9b7c85c0428ce20120f6fe
navi-singh <33882768+navi-singh@users.noreply.github.com>
2024-09-02 22:12:42 -0500

    Update 0060_PermutationSequence.java

--- 0060_PermutationSequence.java
+++ 0060_PermutationSequence.java
@@ -1,5 +1,29 @@
 import java.util.ArrayList;
+/**
+The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
 
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+Example 1:
+Input: n = 3, k = 3
+Output: "213"
+Example 2:
+
+Input: n = 4, k = 9
+Output: "2314"
+Example 3:
+
+Input: n = 3, k = 1
+Output: "123"
+*/
 public class LC60_PermutationSequence {
     // Need to find k/n! where n! will be total permutations
     public String getPermutation(int n, int k) {
