dacedffe21de0b3ab78d2b11d94d39b80a4675e9
navi-singh <navinderjeet@gmail.com>
2024-11-30 13:56:11 -0600

    intellij related changes

--- JavaFiles/LC_Main.java
+++
@@ -1,9 +0,0 @@
-public class LC_Main {
-
-    public static void main(String[] args) {
-        LC_0215_KthLargestElementInArray lc = new LC_0215_KthLargestElementInArray();
-        int[] nums = {3,2,1,5,6,4};
-
-        System.out.println(lc.findKthLargest(nums, 2));
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+
+  LC_0215_KthLargestElementInArray lc = new LC_0215_KthLargestElementInArray();
+  int[] nums = {3, 2, 1, 5, 6, 4};
+
+        System.out.println(lc.findKthLargest(nums,2));
+}
+}
